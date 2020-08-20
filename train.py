4bf676676b9f76fe09b34f84c36786403da16abd
Kartheek Akella <sukruthkartheek@gmail.com>
2020-08-20 08:36:10 +0530

    Update train to use GPU in debug mode.

--- train.py
+++ train.py
@@ -21,7 +21,8 @@ parser.add_argument('--valid', type=float, default=0.2, help='fraction of data t
 
 parser.add_argument('--vocab_size', type=int, default=25_000, help='vocabulary size of text field, default: 25,000')
 parser.add_argument('--pretrained', type=str, default='glove.6B.100d', help='pretrained vectors to be used for '
-                                                                            'Embedding layer, default: \"glove.6B.100d\"')
+                                                                            'Embedding layer, default: '
+                                                                            '\"glove.6B.100d\"')
 parser.add_argument('--dropout_rate', type=float, default=0.5, help='dropout rate to avoid overfitting, default: 0.5')
 parser.add_argument('--embed_dim', type=int, default=100, help='dimensions of embedding layer, default: 100')
 parser.add_argument('--hidden_dim', type=int, default=256, help='number of hidden dim for RNN layers, default: 256')
@@ -31,10 +32,11 @@ parser.add_argument('--model', type=str, default='fast', help='model architectur
 parser.add_argument('--conv_out_channels', type=int, default=100, help='Conv layer out channels, default: 100')
 parser.add_argument('--filter_size', type=int, default=2, help='Default is bigram, set to n-gram')
 parser.add_argument('--debug', type=bool, default=False, help='run the model in fast_dev_run mode, default: False')
-parser.add_argument('--overfit_test', type=int, default=0, help='number of batches on which overfit test to be ran, default: 0')
+parser.add_argument('--overfit_test', type=int, default=0, help='number of batches on which overfit test to be ran, '
+                                                                'default: 0')
 parser.add_argument('--seed', type=int, default=69, help='seed value for reproducable results, default: 69')
 
-## Add further hyper-parameters here.
+# Add further hyper-parameters here.
 
 hparams = parser.parse_args()
 
@@ -84,24 +86,24 @@ if __name__ == '__main__':
     print(f'The model has {hparams.parameters:,} trainable parameters')
 
     if hparams.debug:
-        trainer = pl.Trainer(fast_dev_run=True)
+        trainer = pl.Trainer(fast_dev_run=True, gpus=hparams.n_gpus)
     elif hparams.overfit_test > 0:
-        trainer = pl.Trainer(overfit_batches=hparams.overfit_test, max_epochs=hparams.epochs)
+        trainer = pl.Trainer(overfit_batches=hparams.overfit_test, max_epochs=hparams.epochs, gpus=hparams.n_gpus)
     else:
         trainer = pl.Trainer(max_epochs=hparams.epochs, gpus=hparams.n_gpus)
 
     print('Training...')
     trainer.fit(model, train_dataloader=train_iterator, val_dataloaders=valid_iterator)
 
-    if hparams.debug or hparams.overfit_test>0:
+    if hparams.debug or hparams.overfit_test > 0:
         print('Debug session complete')
     else:
         print('Saving Vocab...')
         folder = "./pretrained/" + hparams.model
         Path(folder).mkdir(parents=True, exist_ok=True)
 
-        save_vocab(TEXT.vocab, folder+'/text.pkl')
-        save_vocab(LABEL.vocab, folder+'/label.pkl')
+        save_vocab(TEXT.vocab, folder + '/text.pkl')
+        save_vocab(LABEL.vocab, folder + '/label.pkl')
 
         print('Testing...')
         trainer.test(test_dataloaders=test_iterator)
